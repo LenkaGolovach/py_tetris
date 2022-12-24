9db1b86dfc3fe0c7a4d9bed389833c8c22038e7f
LenkaGolovach <karamba290@gmail.com>
2022-12-24 05:15:16 +0300

    next creating

--- main-tkinter-ok.py
+++ main-tkinter-ok.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randrange, choice
 
 w, h = 10,20
 tile = 45
@@ -27,15 +28,29 @@ canvas.create_image(0, 0, anchor=NW, image=img_obj1)
 
 
 grid = [canvas.create_rectangle(x * tile, y * tile, x * tile + tile, y * tile + tile) for x in range(w) for y in range(h)]
-print(grid)
 for item in grid:
     canvas.move(item, 20, 20)
 
+score = 0
+record = "0"
 
-canvas.create_text(200,500,text="TETRIS", font=("Arial", 40),fill="white")
+canvas.create_text(505, 30, text="TETRIS", font=("Arial", 45),fill="red", anchor=NW)
+canvas.create_text(535, 780, text="score:", font=("Arial", 45),fill="blue", anchor=NW)
+canvas.create_text(550, 840, text=str(score), font=("Arial", 45),fill="blue", anchor=NW)
+canvas.create_text(525, 650, text="record:", font=("Arial", 45),fill="gold", anchor=NW)
+canvas.create_text(550, 710, text=record, font=("Arial", 45),fill="gold", anchor=NW)
 
-#canvas.create_rectangle(420,120,480,480, fill="darkgreen", outline="")
-#canvas.create_text(200,500,text="Hello World!", font=("Arial", 40),fill="white")
+get_color = lambda : (randrange(30, 256), randrange(30, 256), randrange(30, 256))
 
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+print(rgb_to_hex(get_color()))
+
+for item in grid:
+    canvas.itemconfigure(item, fill=rgb_to_hex(get_color()))
+
+for item in grid:
+    canvas.itemconfigure(item, fill="")
 
 tk.mainloop()
