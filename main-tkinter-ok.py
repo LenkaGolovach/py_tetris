ee736dee4d4d7ab472bdc7d38780965da0486073
LenkaGolovach <karamba290@gmail.com>
2022-12-24 06:45:57 +0300

    prelast time

--- main-tkinter-ok.py
+++ main-tkinter-ok.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import randrange, choice
+from copy import deepcopy
 
 w, h = 10,20
 tile = 45
@@ -23,20 +24,26 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=res[0], height=res[1], bg="red", highlightthickness=0)
 canvas.pack()
 
+sc_game = Canvas(tk, width=w * tile + 1, height=h * tile + 1, bg="purple", highlightthickness=0)
+sc_game.place(x=20, y=20, anchor=NW)
+
 img_obj1 = PhotoImage(file="img/vasya.png")
 canvas.create_image(0, 0, anchor=NW, image=img_obj1)
 
+img_obj2 = PhotoImage(file="img/krol.png")
+sc_game.create_image(0, 0, anchor=NW, image=img_obj2)
 
-grid = [canvas.create_rectangle(x * tile, y * tile, x * tile + tile, y * tile + tile) for x in range(w) for y in range(h)]
-for item in grid:
-    canvas.move(item, 20, 20)
+
+grid = [sc_game.create_rectangle(x * tile, y * tile, x * tile + tile, y * tile + tile) for x in range(w) for y in range(h)]
+# for item in grid:
+#     sc_game.move(item, 20, 20)
 
 score = 0
 record = "0"
 
 canvas.create_text(505, 30, text="TETRIS", font=("Arial", 45),fill="red", anchor=NW)
 canvas.create_text(535, 780, text="score:", font=("Arial", 45),fill="blue", anchor=NW)
-canvas.create_text(550, 840, text=str(score), font=("Arial", 45),fill="blue", anchor=NW)
+canvas.create_text(550, 840, text=str(score), font=("Arial", 45),fill="black", anchor=NW)
 canvas.create_text(525, 650, text="record:", font=("Arial", 45),fill="gold", anchor=NW)
 canvas.create_text(550, 710, text=record, font=("Arial", 45),fill="gold", anchor=NW)
 
@@ -45,12 +52,35 @@ get_color = lambda : (randrange(30, 256), randrange(30, 256), randrange(30, 256)
 def rgb_to_hex(rgb):
     return '#%02x%02x%02x' % rgb
 
-print(rgb_to_hex(get_color()))
+figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
+               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
+               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
+               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
+
+figures = [[(x + w // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
+
+figure, next_figure = deepcopy(choice(figures)), deepcopy(choice(figures))
+color, next_color = get_color(), get_color()
+
+#draw figure
+for i in range(4):
+    figure_rect_x = figure[i][0] * tile
+    figure_rect_y = figure[i][1] * tile
+    sc_game.create_rectangle(figure_rect_x, figure_rect_y, figure_rect_x + tile, figure_rect_y + tile, fill = rgb_to_hex(color))
+
+#draw next figure
+for i in range(4):
+    figure_rect_x = next_figure[i][0] * tile + 380
+    figure_rect_y = next_figure[i][1] * tile + 185
+    canvas.create_rectangle(figure_rect_x, figure_rect_y, figure_rect_x + tile, figure_rect_y + tile, fill = rgb_to_hex(next_color))
 
 for item in grid:
-    canvas.itemconfigure(item, fill=rgb_to_hex(get_color()))
+    sc_game.itemconfigure(item, fill=rgb_to_hex(get_color()))
 
 for item in grid:
-    canvas.itemconfigure(item, fill="")
+    sc_game.itemconfigure(item, fill="")
 
 tk.mainloop()
